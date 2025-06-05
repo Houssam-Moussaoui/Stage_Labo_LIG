55689eed9715c94f7244fd294180ce484facc9da
Houssam Moussaoui <moussaoh@grenoble-inp.org>
2025-06-05 11:21:46 +0200

    mise en place d'un systeme de cache pour sauvegarder les pages HTML

--- src/api_parcoursup.py
+++ src/api_parcoursup.py
@@ -1,48 +1,54 @@
 import requests
 from fiche_formation import extraction_donnee_fiche
 
-data = requests.get("https://data.enseignementsup-recherche.gouv.fr/api/explore/v2.1/catalog/datasets/fr-esr-cartographie_formations_parcoursup/exports/json").json()
-count_formation = len(data)
-N = 1
 
-for i in range(count_formation):
-    formation = data[i]
+def main():
 
-    annee = formation.get('annee')
-    etab_uai = formation.get('etab_uai')
-    etab_nom = formation.get('etab_nom')
-    tc = formation.get('tc')
-    tf = formation.get('tf', [None])[0] if formation.get('tf') is not None else None
-    nm = formation.get('nm', [None])[0] if formation.get('nm') is not None else None
-    fl = formation.get('fl', [None])[0] if formation.get('fl') is not None else None
-    app = formation.get('app', [None])[0] if formation.get('app') is not None else None
-    int_field = formation.get('int')  
-    amg = formation.get('amg', [None])[0] if formation.get('amg') is not None else None
-    aut = formation.get('aut', [None])[0] if formation.get('aut') is not None else None
-    region = formation.get('region')
-    departement = formation.get('departement')
-    commune = formation.get('commune')
-    fiche = formation.get('fiche')
-
-    extraction_donnee_fiche(fiche)
+    data = requests.get("https://data.enseignementsup-recherche.gouv.fr/api/explore/v2.1/catalog/datasets/fr-esr-cartographie_formations_parcoursup/exports/json").json()
+    count_formation = len(data)
+    print( "len est : >>> ",count_formation)
+    N = 10
 
 
-    dataivz = formation.get('dataviz')  
-    etab_url = formation.get('etab_url')
+    for i in range(N):
+        formation = data[i]
 
-    gps = formation.get('etab_gps', {})
-    lon = gps.get('lon')
-    lat = gps.get('lat')
+        annee = formation.get('annee')
+        etab_uai = formation.get('etab_uai')
+        etab_nom = formation.get('etab_nom')
+        tc = formation.get('tc')
+        tf = formation.get('tf', [None])[0] if formation.get('tf') is not None else None
+        nm = formation.get('nm', [None])[0] if formation.get('nm') is not None else None
+        fl = formation.get('fl', [None])[0] if formation.get('fl') is not None else None
+        app = formation.get('app', [None])[0] if formation.get('app') is not None else None
+        int_field = formation.get('int')  
+        amg = formation.get('amg', [None])[0] if formation.get('amg') is not None else None
+        aut = formation.get('aut', [None])[0] if formation.get('aut') is not None else None
+        region = formation.get('region')
+        departement = formation.get('departement')
+        commune = formation.get('commune')
+        fiche = formation.get('fiche')
 
-    nmc = formation.get('nmc')
-    gta = formation.get('gta')
-
-    # Exemple d'affichage 
-    #print(f" {i+1} | {annee} | {etab_nom} | {nm} | {app} | {commune} ({departement})    |  {fiche }")
-    print(i)
+        extraction_donnee_fiche(fiche)
 
 
+        dataivz = formation.get('dataviz')  
+        etab_url = formation.get('etab_url')
+
+        gps = formation.get('etab_gps', {})
+        lon = gps.get('lon')
+        lat = gps.get('lat')
+
+        nmc = formation.get('nmc')
+        gta = formation.get('gta')
+
+        # Exemple d'affichage 
+        #print(f" {i+1} | {annee} | {etab_nom} | {nm} | {app} | {commune} ({departement})    |  {fiche }")
+        #print(i)
 
 
 
 
+
+
+main()
--- src/fiche_formation.py
+++ src/fiche_formation.py
@@ -1,16 +1,66 @@
 import requests
 from bs4 import BeautifulSoup
+import os 
+import hashlib
 
 
-def extraction_donnee_fiche(fiche):
-    reponse = requests.get(fiche)
 
-    if(reponse.status_code==200):
-        html = BeautifulSoup(reponse.text,"lxml")
+# Chemin absolu vers le dossier 'src'
+SRC_DIR = os.path.dirname(__file__)
+# Racine du projet (un niveau au-dessus de 'src')
+PROJECT_ROOT = os.path.abspath(os.path.join(SRC_DIR, os.pardir))
 
-        #print(html.text)
-    else:
-        print("Erreur dans la lecture de la fiche  : ",fiche)
-        return -1
+# Dossier de cache HTML
+CACHE_DIR = os.path.join(PROJECT_ROOT, "bd_html")
+os.makedirs(CACHE_DIR, exist_ok=True)
+
+
+
+
+def url_to_filename(url: str) -> str:
+    """
+    Transforme l'URL en un nom de fichier unique et valide, via un hash MD5.
+    """
+    h = hashlib.md5(url.encode("utf-8")).hexdigest()
+    return f"{h}.html"
+
+
+
+def extraction_donnee_fiche(fiche,):
+
+
+    filename = url_to_filename(fiche)
+    file_path = os.path.join(CACHE_DIR, filename)
+
+    html =None
+
+
+
+
+    if(os.path.exists(file_path)): # on vérifie si le fichier existe déjà dans notre base de donnée (bd_html)
+        print(f"fichier {file_path} existe déjà dans bd_html")
+        with open(file_path,"r",encoding='utf-8') as f:  # si oui , on lit directement le fichier associé 
+            html = f.read()
+    
+    else: #sinon on va le lire avec requests et le stocker dans bd_html , et le marquer dans notre dico ens_fiches
+        print(f"fichier {file_path}n'existe pas dans bd_html")
+
+        reponse = requests.get(fiche)
+
+        if(reponse.status_code==200):
+            html = BeautifulSoup(reponse.text,"lxml")
+
+            with open(file_path,'a',encoding='utf-8') as f:
+                f.write(html.text)
+
+
+            #print(html.text)
+        else:
+            print("Erreur dans la lecture de la fiche  : ",fiche)
+            return -1
+
+
+    
+    
 
     
